a13bffa6d8e040833d976847148d78a13545dcaf
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2023-09-28 09:21:13 +0200

    practica2

--- Tema2/Practicas/Practica02T2/js/script.js
+++ Tema2/Practicas/Practica02T2/js/script.js
@@ -1,4 +1,22 @@
 function tableSize(){
     let numeroColumnas;
     let numeroFilas;
+
+    alert("En esta web se te permite asignar el tamaño para genera una tabla.");
+    numeroColumnas = prompt("Introduce el número de columnas.");
+    numeroFilas = prompt("Introduce el número de fileas.");
+
+    document.write("<table>");
+
+    for(let i = 1; i<=numeroColumnas; i++){
+        document.write("<tr>");
+        for(let j = 1; j<=numeroFilas; j++){
+            document.write("<td>" + "C" + j + "F" + i + "</td>");
+        }
+        document.write("</tr>");
+    }
+
+    document.write("</table>");
 }
+
+tableSize();
