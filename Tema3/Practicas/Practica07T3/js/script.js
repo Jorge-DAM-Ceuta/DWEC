c36660f43234a75e7286a0d98dceee896de8a396
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2023-10-03 22:11:07 +0200

    dni... vaya tela

---
+++ Tema3/Practicas/Practica07T3/js/script.js
@@ -0,0 +1,29 @@
+/*if(dni == undefined){
+    print("Se ha pulsado cancelar");
+}else if(dni == ""){
+    print("Se ha pulsado aceptar con el campo vacío");
+}else if(dni.length == 9){
+    print("Se ha introducido: " + dni);
+}*/
+
+var dni = "dni"; 
+var letrasAceptadas = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+var comprobarNumeros = true;
+var comprobarLetra = true;
+var numerosDNI = dni.substring(0, 9);
+
+do {
+
+    dni = prompt("Introduce tu DNI:"); 
+
+    for(let i = 0; i<letrasAceptadas.length; i++){
+        if(dni[dni.length-1] != letrasAceptadas[i]){
+            comprobarLetra = false;
+        }
+    }
+    
+    if(isNaN(numerosDNI)){
+        comprobarNumeros = false;
+    }
+
+}while(dni == undefined || dni == "" || dni == 0 || dni.length != 9 || comprobarLetra == false || comprobarNumeros == false);
