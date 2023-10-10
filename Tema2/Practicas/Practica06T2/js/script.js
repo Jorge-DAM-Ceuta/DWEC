7e79a4291df8e71426bb6471727164e118ae997e
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2023-10-10 20:19:55 +0200

    PracticaColores

---
+++ Tema2/Practicas/Practica06T2/js/script.js
@@ -0,0 +1,17 @@
+
+var numeroCorrecto = (Math.random*100) + 1;
+var numeroIntroducido = "";
+
+    
+do{
+
+    numeroIntroducido = prompt("Adivina el número del 1 al 100:");
+
+    if(numeroIntroducido == numeroCorrecto){
+        prompt()
+    }
+
+}while(numeroIntroducido != numeroCorrecto)
+
+    
+
