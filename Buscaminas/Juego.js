9e09b717db3a55586b7690a2a7c956312bc22816
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2023-11-24 20:30:44 +0100

    Proyecto EVA 1

---
+++ Buscaminas/Juego.js
@@ -0,0 +1,34 @@
+var tableroSize = prompt("Introduce un tamaño para el tablero: ");
+var numeroMinas = prompt("Introduce un número de minas a colocar: ");
+
+function nMinasPos(tablero, fila, columna){
+
+}
+
+function colocaRelleno(tablero){
+
+}
+
+function colocaMinas(tablero, minas){
+
+}
+
+function dibujaTableroWeb(tablero){
+
+}
+
+function getN_Filas(){
+
+}
+
+function getN_Columnas(){
+
+}
+
+function getN_Minas(nFilas, nColumnas){
+
+}
+
+function nMinasPos(tablero, filas, columnas){
+
+}
