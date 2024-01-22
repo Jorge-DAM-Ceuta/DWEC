41443a0b4dbb682b9b17d13282d80311a17bc13c
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-01-22 21:20:40 +0100

    Terminado

--- Proyectos/PROYECTO01EVA2_API/script/script.js
+++ Proyectos/PROYECTO01EVA2_API/script/script.js
@@ -1,18 +1,40 @@
 function mostrarArchivo(ev){
     var inputArchivo = document.getElementById("archivo");
-    var tituloArchivo = document.getElementById("tituloArchivo");
-    var textoArchivo = document.getElementById("textoArchivo");
     
+    //Se obtiene el archivo seleccionado en el input.
     var archivoSeleccionado = inputArchivo.files[0];
+    
+    //Si el archivo se ha recogido correctamente y es distinto de null.
+    if(archivoSeleccionado != null){
+        //Creamos un elemento h2 para escribir el título del archivo:
+        let tituloArchivo = document.createElement("h2");
 
-    tituloArchivo.textContent = `Título: ${archivoSeleccionado.name}`;
+        //Escribimos el título del archivo mediante la propiedad name en el elemento.
+        tituloArchivo.textContent = `Título: ${archivoSeleccionado.name}`;
 
-    //Declaramos un nuevo objeto FileReader para leer el archivo obtenido.
-    var fileReader = new FileReader();
+        //Añadimos el elemento al body:
+        document.body.appendChild(tituloArchivo);
 
-    fileReader.onload = (evt) => {
-        textoArchivo.textContent = `Contenido: <br/>${evt.target.result}`;
+        //Declaramos un nuevo objeto FileReader para leer el archivo obtenido.
+        var fileReader = new FileReader();
+        
+        /*Este método realiza una lectura del texto del archivo, cuando lo lee se 
+        activa el evento anterior con el contenido del archivo que recibe como evt.*/
+        fileReader.readAsText(archivoSeleccionado);
+        
+        //Este evento se activa cuando se ha cargado el contenido del archivo mediante readAsText(). 
+        fileReader.onload = (evt) => {
+            //Creamos un elemento p para escribir el contenido del archivo:
+            let textoArchivo = document.createElement("p");
+
+            /*Con evt se obtienen las informaciones del evento, en este caso el contenido del archivo. 
+            Con las que se permite acceder al contenido del archivo mediante target.result*/
+            textoArchivo.textContent = `Contenido: ${evt.target.result}`;
+
+            //Añadimos el elemento al body:
+            document.body.appendChild(textoArchivo);
+        }
+
+        
     }
-
-    fileReader.readAsText(archivoSeleccionado);
 }
