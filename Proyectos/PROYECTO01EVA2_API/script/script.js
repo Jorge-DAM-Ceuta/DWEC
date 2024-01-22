fc861566ab5229a1c071793d515d4ce41aa1b25b
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-01-22 20:58:16 +0100

    Cambios

--- Proyectos/PROYECTO01EVA2_API/script/script.js
+++ Proyectos/PROYECTO01EVA2_API/script/script.js
@@ -1,3 +1,18 @@
-function mostrarArchivo(){
+function mostrarArchivo(ev){
+    var inputArchivo = document.getElementById("archivo");
+    var tituloArchivo = document.getElementById("tituloArchivo");
+    var textoArchivo = document.getElementById("textoArchivo");
     
+    var archivoSeleccionado = inputArchivo.files[0];
+
+    tituloArchivo.textContent = `Título: ${archivoSeleccionado.name}`;
+
+    //Declaramos un nuevo objeto FileReader para leer el archivo obtenido.
+    var fileReader = new FileReader();
+
+    fileReader.onload = (evt) => {
+        textoArchivo.textContent = `Contenido: <br/>${evt.target.result}`;
+    }
+
+    fileReader.readAsText(archivoSeleccionado);
 }
