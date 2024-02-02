7e56f0f740a861aedf768c346f92bc21e1fb3509
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-02-02 18:29:25 +0100

    No funciona...

--- Tema6/Practicas/Practica09T6_EVA2/scripts/script.js
+++ Tema6/Practicas/Practica09T6_EVA2/scripts/script.js
@@ -1,57 +1,50 @@
 function permiteCookie() {
     // Se setea la cookie a true con visitas a 1
-    document.cookie = `permiteCookie=true; visitas=1; max-age=${60 * 60 * 24 * 365};`;
-    
-    //Se oculta el panel de las cookies
-    document.getElementById("panelCookies").classList.add("hidden");
+    document.cookie = "permiteCookie=true; max-age=60*60*24*365";
+    document.cookie = "visitas=1; max-age=60*60*24*365";
 
-    // Mostramos las visitas y el botón para cerrar sesión
-    muestraVisitas();
-    mostrarCerrarSesion();
-}
+    //Se oculta el panel de las cookies y se muestra el resultado y el botón de cerrar sesión
+    document.getElementById("panelCookies").setAttribute("hidden", "");
+    document.getElementById("resultado").removeAttribute("hidden");
+    document.getElementById("botonCierraSesion").removeAttribute("hidden");
 
-function rechazaCookie() {
-    // Se setea la cookie a false
-    document.cookie = `permiteCookie=false; visitas=0; max-age=${60 * 60 * 24 * 365};`;
-
-    // Se oculta el panel de las cookies
-    document.getElementById("panelCookies").classList.add("hidden");
-
-    // Asignamos el valor oportuno en el método muestraVisitas().
-    muestraVisitas();
-}
-
-function mostrarCerrarSesion() {
-    let botonCierraSesion = document.createElement("button");
-    botonCierraSesion.textContent = "Cerrar sesión";
-    botonCierraSesion.id = "botonCierraSesion";
-
-    // Se añade el botón al documento
-    document.body.appendChild(botonCierraSesion);
-
-    // Se agrega el evento de clic al nuevo botón que setea la cookie a false y muestra el panel de las cookies
-    botonCierraSesion.addEventListener("click", function(){
-        setCookie("permiteCookie", "false");
-        document.getElementById("panelCookies").classList.remove("hidden");
-    });
-}
-
-function muestraVisitas() {
+    let numeroVisitas = document.getElementById("numeroVisitas");
     let cookies = document.cookie.split(';');
 
     for (let i = 0; i < cookies.length; i++) {
         let cookie = cookies[i].trim();
-    
-        if (cookie.indexOf("visitas=") == 0) {
+
+        if (cookie.indexOf("visitas=") === 0) {
             // Obtienes el valor de la cookie "visitas"
             let valorVisitas = cookie.substring("visitas=".length);
-            console.log(`El valor actual de la cookie "visitas" es: ${valorVisitas}`);
+            numeroVisitas.textContent = valorVisitas;
         }
     }
+}
+
+function rechazaCookie() {
+    // Se setea la cookie a false
+    document.cookie = "permiteCookie=false";
+
+    // Se oculta el panel de las cookies
+    document.getElementById("panelCookies").setAttribute("hidden", "");
+    document.getElementById("resultado").setAttribute("hidden", "");
+    document.getElementById("botonCierraSesion").setAttribute("hidden", "");
+}
+
+function reestablecer() {
+    // Se setea la cookie a false
+    document.cookie = "permiteCookie=false";
+    
+    // Se oculta el panel de las cookies
+    document.getElementById("panelCookies").removeAttribute("hidden");
+    document.getElementById("resultado").setAttribute("hidden", "");
+    document.getElementById("botonCierraSesion").setAttribute("hidden", "");
 }
 
 function iniciaListeners(){
     // Agregamos eventos a los botones
     document.getElementById("botonPermite").addEventListener("click", permiteCookie);
     document.getElementById("botonRechaza").addEventListener("click", rechazaCookie);
+    document.getElementById("botonCierraSesion").addEventListener("click", reestablecer);
 }
