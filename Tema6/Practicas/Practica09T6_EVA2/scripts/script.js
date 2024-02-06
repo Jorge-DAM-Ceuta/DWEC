7d6b31b3ed9f4347b150e83daf14d61d4a1cc7c7
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-02-06 13:24:49 +0100

    Sigue sin funcionar........

--- Tema6/Practicas/Practica09T6_EVA2/scripts/script.js
+++ Tema6/Practicas/Practica09T6_EVA2/scripts/script.js
@@ -1,38 +1,47 @@
-function permiteCookie() {
+function permiteCookie(){
     // Se setea la cookie a true con visitas a 1
-    document.cookie = "permiteCookie=true; max-age=60*60*24*365";
-    document.cookie = "visitas=1; max-age=60*60*24*365";
-
+    document.cookie = "permiteCookie=1; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/;";
+    
     //Se oculta el panel de las cookies y se muestra el resultado y el botón de cerrar sesión
     document.getElementById("panelCookies").setAttribute("hidden", "");
     document.getElementById("resultado").removeAttribute("hidden");
 
-    let numeroVisitas = document.getElementById("numeroVisitas");
-    let cookies = document.cookie.split(';');
+    let numeroVisitas = getCookieValue("permiteCookie");
+    let muestraVisitas = document.getElementById("numeroVisitas");
 
-    for (let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i].trim();
+    console.log(document.cookie);
 
-        if (cookie.indexOf("visitas=") === 0) {
-            // Obtienes el valor de la cookie "visitas"
-            let valorVisitas = cookie.substring("visitas=".length);
-            numeroVisitas.textContent = valorVisitas;
-        }
-    }
+    muestraVisitas.textContent = numeroVisitas;
+    
 }
 
-function rechazaCookie() {
+function getCookieValue(nombre){
+    let cookies = document.cookie.split(";"); 
+
+    for(let i = 0; i < cookies.length; i++){
+
+        let cookie = cookies[i].trim(); 
+
+        if(cookie.startsWith(nombre + "=")){
+            return cookie.substring(nombre.length + 1); 
+        }
+    }
+
+    return null; 
+}
+
+function rechazaCookie(){
     // Se setea la cookie a false
-    document.cookie = "permiteCookie=false";
+    document.cookie = "permiteCookie=0; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/;";
 
     // Se oculta el panel de las cookies y el resultado
     document.getElementById("panelCookies").setAttribute("hidden", "");
     document.getElementById("resultado").setAttribute("hidden", "");
 }
 
-function reestablecer() {
+function reestablecer(){
     // Se setea la cookie a false
-    document.cookie = "permiteCookie=false";
+    document.cookie = "permiteCookie=0; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/;";
     
     // Se oculta el panel de las cookies
     document.getElementById("panelCookies").removeAttribute("hidden");
@@ -44,4 +53,7 @@ function iniciaListeners(){
     document.getElementById("botonPermite").addEventListener("click", permiteCookie);
     document.getElementById("botonRechaza").addEventListener("click", rechazaCookie);
     document.getElementById("botonCierraSesion").addEventListener("click", reestablecer);
+
+    //Si la cookie es true y se carga el documento se suma la cookie visitas en 1.
+    //document.addEventListener("DOMContentLoaded", sumarVisita);
 }
