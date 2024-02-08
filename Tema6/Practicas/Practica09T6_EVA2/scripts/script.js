e2ff09cfac10410a1c967027b7ff5f8edd4ec6f1
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-02-08 19:04:22 +0100

    Terminado - no funciona

--- Tema6/Practicas/Practica09T6_EVA2/scripts/script.js
+++ Tema6/Practicas/Practica09T6_EVA2/scripts/script.js
@@ -1,6 +1,6 @@
 function permiteCookie(){
     // Se setea la cookie a true con visitas a 1
-    document.cookie = "visitas=1; max-age=60*60*24*365; path=/;";
+    document.cookie = "visitas=1; max-age=" + (60*60*24*365) + "; path=/;";
     
     // Se oculta el panel de las cookies y se muestra el resultado y el botón de cerrar sesión
     document.getElementById("panelCookies").setAttribute("hidden", "");
@@ -51,13 +51,15 @@ function reestablecer(){
 function aumentarContadorVisitas(){
     // Obtenemos el valor de la cookie.
     let valorCookie = getCookieValue("visitas");
+    let contador = document.getElementById("numeroVisitas");
  
     // Si la cookie existe y su valor es mayor que 0 y distinto de null se obtiene su valor y se le incrementa.
     if(valorCookie != null && parseInt(valorCookie) > 0){
         let nuevoValor = parseInt(valorCookie) + 1;
        
         // Se actualiza la cookie con el nuevo valor.
-        document.cookie = "visitas=" + nuevoValor + "; max-age=60*60*24*365; path=/;";
+        document.cookie = "visitas=" + nuevoValor + "; path=/;";
+        contador.textContent = nuevoValor;
     }
  }
  
@@ -68,15 +70,12 @@ function iniciaListeners(){
     document.getElementById("botonRechaza").addEventListener("click", rechazaCookie);
     document.getElementById("botonCierraSesion").addEventListener("click", reestablecer);
 
-    // Si al cargar el documento el valor de la cookie es mayor que 0, está seteada
-    document.addEventListener("DOMContentLoaded", function(){
-        if(getCookieValue("visitas").length > 0){
-            // Ya no se muestra el panel de las cookies y se muestra el resultado.
-            document.getElementById("panelCookies").setAttribute("hidden", "");
-            document.getElementById("resultado").removeAttribute("hidden");
+    if(parseInt(getCookieValue("visitas")) > 0){
+        // Ya no se muestra el panel de las cookies y se muestra el resultado.
+        document.getElementById("panelCookies").setAttribute("hidden", "");
+        document.getElementById("resultado").removeAttribute("hidden");
 
-            // Se le suma a uno la cantidad de visitas.
-            aumentarContadorVisitas();
-        }
-    });
+        // Se le suma a uno la cantidad de visitas.
+        aumentarContadorVisitas();
+    }
 }
