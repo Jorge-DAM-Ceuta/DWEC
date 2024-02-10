795da7f0ec5fb4d131e3f2413bbbbd872b12953d
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-02-10 23:55:15 +0100

    Inicio proyecto

---
+++ Proyectos/PROYECTO04EVA2_Eventos/js/script.js
@@ -0,0 +1,15 @@
+function iniciaJuego(){
+    let contenedorPuzzle = document.getElementById("puzzle");
+    let piezas = [];
+    let intentos = 0;
+
+
+}
+
+function cargarPiezasAleatoriamente(){
+
+}
+
+function comprobarPuzzle(){
+    
+}
