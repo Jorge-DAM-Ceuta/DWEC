2b3e6e502f3efa2e769df4c05fa566b39ac37c90
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-02-11 18:28:26 +0100

    Comprobar posiciones piezas

--- Proyectos/PROYECTO04EVA2_Eventos/js/script.js
+++ Proyectos/PROYECTO04EVA2_Eventos/js/script.js
@@ -1,14 +1,18 @@
+//Esta función contiene el contenedor de las piezas, el array que contendrá cada elemento del contenedor y un número de intentos.
 function iniciaJuego(){
     let contenedorPuzzle = document.getElementById("puzzle");
     let piezas = [];
     let intentos = 0;
 
+    //Se llama a la función cargarPuzzle.
     cargarPuzzle();
 
+    //Esta función está dentro de la función iniciaJuego para poder acceder a sus variables y trabajar con el array piezas.
     function cargarPuzzle(){
+        //Array que contendrá los valores.
         let numeros = [];
         
-        //Rellenamos el array de numeros de números del 1 al 16.
+        //Rellenamos el array de números de números del 1 al 16.
         for(let i = 0; i < 16; i++){
             numeros[i] = i + 1;
         }
@@ -32,14 +36,17 @@ function iniciaJuego(){
     
             //Si el contenido de la pieza es igual al contador + 1 (para igualar los valores con el índice del array) está en su posición correcta.
             if(parseInt(pieza.textContent) == j + 1){
+                //Añadimos un color verde a la pieza.
                 pieza.classList.add("pieza_verde");
     
             //Si el valor de la pieza - 1 (para igualar los valores con el índice del array) entre 4 es igual a la fila actual o el resto de su valor entre 4 es igual a la columna actual.
-            }else if((parseInt(pieza.textContent) - 1) / 4 == filaActual || (parseInt(pieza.textContent) - 1) % 4 == columnaActual){
+            }else if(parseInt((parseInt(pieza.textContent) - 1) / 4) == filaActual || (parseInt(pieza.textContent) - 1) % 4 == columnaActual){
+                //Añadimos un color naranja a la pieza.
                 pieza.classList.add("pieza_naranja");
     
             //Si no se ha dado ninguno de los casos la pieza no está ni en su columna, ni su fila ni en la posición correcta.
             }else{
+                //Añadimos un color gris a la pieza.
                 pieza.classList.add("pieza_gris");
             }
     
@@ -71,10 +78,13 @@ function iniciaJuego(){
                 let indicePiezaArrastrada = 0;
                 let indicePiezaSoltada = 0;
     
-                //Recorremos el array de piezas y obtenemos el índice de la pieza que hemos arrastrado y el de la pieza donde hemos soltado la anterior.
+                //Recorremos el array de piezas.
                 for(let i = 0; i < piezas.length; i++){
+                    //Si encontramos el mismo valor en el array que el del elemento arrastrado, obtenemos su índice.
                     if(piezas[i].textContent == valorArrastrado){
                         indicePiezaArrastrada = i;
+
+                    //Si encontramos el mismo valor en el array que el del elemento donde hemos soltado, obtenemos su índice.
                     }else if(piezas[i].textContent == valorSoltado){
                         indicePiezaSoltada = i;
                     }
@@ -85,6 +95,8 @@ function iniciaJuego(){
                 piezas[indicePiezaArrastrada].textContent = piezas[indicePiezaSoltada].textContent;
                 piezas[indicePiezaSoltada].textContent = respaldo;
     
+                //Llamaremos a esta función cada vez que se arrastre un elemento para actualizar los colores de cada pieza y comprobar si se ha terminado el puzzle.
+                comprobarPuzzle();
             });
             
             //Añadimos la pieza al array de piezas.
@@ -94,8 +106,63 @@ function iniciaJuego(){
             contenedorPuzzle.appendChild(pieza);
         }
     }
+
+    function comprobarPuzzle(){
+        /*Con esta variable comprobaremos si todas las piezas están en sus correspondientes 
+        posiciones para terminar el juego y mostrar el número de intentos. Empieza valiendo
+        true, si en alguna comprobación de una pieza no está en su posicion correcta, esta
+        se setea a false.*/
+        let todasCorrectas = true;
+
+        //Aumentamos el contador de intentos por cada pieza que se suelta.
+        intentos++;
+
+        //Recorremos el array de piezas.
+        for(let i = 0; i < piezas.length; i++){
+            //Obtenemos la pieza actual.
+            let pieza = piezas[i];
+
+            //Obtenemos la fila y columna actuales.
+            let filaActual = parseInt(i / 4);
+            let columnaActual = i % 4;
+
+            //Eliminamos todas las clases css que pueda tener el elemento.
+            pieza.classList.remove("pieza_verde", "pieza_naranja", "pieza_gris");
+            
+            //Si el contenido de la pieza es igual al contador + 1 (para igualar los valores con el índice del array) está en su posición correcta.
+            if(parseInt(pieza.textContent) == i + 1){
+                //Añadimos un color verde a la pieza.
+                pieza.classList.add("pieza_verde");
+    
+            //Si el valor de la pieza - 1 (para igualar los valores con el índice del array) entre 4 es igual a la fila actual o el resto de su valor entre 4 es igual a la columna actual.
+            }else if(parseInt((parseInt(pieza.textContent) - 1) / 4) == filaActual || (parseInt(pieza.textContent) - 1) % 4 == columnaActual){
+                //Añadimos un color naranja a la pieza.
+                pieza.classList.add("pieza_naranja");
+
+                //Se setea a false la variable todasCorrectas.
+                todasCorrectas = false;
+
+            //Si no se ha dado ninguno de los casos la pieza no está ni en su columna, ni su fila ni en la posición correcta.
+            }else{
+                //Añadimos un color gris a la pieza.
+                pieza.classList.add("pieza_gris");
+
+                //Se setea a false la variable todasCorrectas.
+                todasCorrectas = false;
+            }
+        }
+
+        //Si todas las piezas están en su posición correspondiente se muestra un mensaje con el número de intentos.
+        if(todasCorrectas == true){
+            let resultadoIntentos = document.createElement("h2");
+            resultadoIntentos.textContent = `!Has resuelto el puzzle en ${intentos} intentos!`;
+
+            document.body.appendChild(resultadoIntentos);
+        }
+    }
 }
 
+//Esta función intercambia posiciones en el array.
 function desordenarPiezas(piezas){
     //Recorremos el array de piezas.
     for(let posicionActual = 0; posicionActual < piezas.length; posicionActual++){
@@ -110,8 +177,4 @@ function desordenarPiezas(piezas){
 
     //Devolvemos las piezas desordenadas.
     return piezas;
-}
-
-function comprobarPuzzle(){
-
 }
