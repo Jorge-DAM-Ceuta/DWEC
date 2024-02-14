e317dee6ae6936a7f3fe8e62f6da90eeb4751ea9
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-02-14 12:29:14 +0100

    Códigos clase

---
+++ Tema8/Actividades/EjemploFetch/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Configuración</title>
+        <style>
+            body { text-align: center; }
+        </style>
+    </head>
+    <body>
+        <?php echo phpinfo(); ?>
+    </body>
+</html>
