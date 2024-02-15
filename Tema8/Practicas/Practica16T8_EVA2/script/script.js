fea4608a47bb202011b132c69779f6ea73669328
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-02-15 18:34:42 +0100

    Práctica 9 arreglada y práctica 16

--- Tema8/Practicas/Practica16T8_EVA2/script/script.js
+++ Tema8/Practicas/Practica16T8_EVA2/script/script.js
@@ -1,18 +1,34 @@
+export function iniciaListeners(){
+    //Agregamos eventos a los botones
+    document.getElementById("botonPermite").addEventListener("click", permiteCookie);
+    document.getElementById("botonRechaza").addEventListener("click", rechazaCookie);
+    document.getElementById("botonCierraSesion").addEventListener("click", reestablecer);
+
+    if(parseInt(getCookieValue("visitas")) > 0){
+        //Ya no se muestra el panel de las cookies y se muestra el resultado.
+        document.getElementById("panelCookies").setAttribute("hidden", "");
+        document.getElementById("resultado").removeAttribute("hidden");
+
+        //Se le suma a uno la cantidad de visitas.
+        aumentarContadorVisitas();
+    }
+}
+
 function permiteCookie(){
-    // Se setea la cookie a true con visitas a 1
+    //Se setea la cookie a true con visitas a 1
     document.cookie = "visitas=1; max-age=" + (60*60*24*365) + "; path=/;";
     
-    // Se oculta el panel de las cookies y se muestra el resultado y el botón de cerrar sesión
+    //Se oculta el panel de las cookies y se muestra el resultado y el botón de cerrar sesión
     document.getElementById("panelCookies").setAttribute("hidden", "");
     document.getElementById("resultado").removeAttribute("hidden");
 
     let numeroVisitas = getCookieValue("visitas");
     let muestraVisitas = document.getElementById("numeroVisitas");
 
-    // Se muestra el valor de la cookie en consola.
+    //Se muestra el valor de la cookie en consola.
     console.log(numeroVisitas);
 
-    // Se escribe el valor en el contador del resultado.
+    //Se escribe el valor en el contador del resultado.
     muestraVisitas.textContent = numeroVisitas;
 }
 
@@ -41,12 +57,12 @@ function rechazaCookie(){
     let muestraVisitas = document.getElementById("numeroVisitas");
     muestraVisitas.textContent = "No has aceptado las cookies";
     
-    // Se "destruye" la cookie.
+    //Se "destruye" la cookie.
     document.cookie = "visitas=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
 function reestablecer(){
-    // Se oculta el panel de las cookies
+    //Se oculta el panel de las cookies
     document.getElementById("panelCookies").removeAttribute("hidden");
     document.getElementById("resultado").setAttribute("hidden", "");
 
@@ -55,33 +71,16 @@ function reestablecer(){
 }
 
 function aumentarContadorVisitas(){
-    // Obtenemos el valor de la cookie.
+    //Obtenemos el valor de la cookie.
     let valorCookie = getCookieValue("visitas");
     let contador = document.getElementById("numeroVisitas");
  
-    // Si la cookie existe y su valor es mayor que 0 y distinto de null se obtiene su valor y se le incrementa.
+    //Si la cookie existe y su valor es mayor que 0 y distinto de null se obtiene su valor y se le incrementa.
     if(valorCookie != null && parseInt(valorCookie) > 0){
         let nuevoValor = parseInt(valorCookie) + 1;
        
-        // Se actualiza la cookie con el nuevo valor.
+        //Se actualiza la cookie con el nuevo valor.
         document.cookie = "visitas=" + nuevoValor + "; path=/;";
         contador.textContent = nuevoValor;
-    }
- }
- 
-
-function iniciaListeners(){
-    // Agregamos eventos a los botones
-    document.getElementById("botonPermite").addEventListener("click", permiteCookie);
-    document.getElementById("botonRechaza").addEventListener("click", rechazaCookie);
-    document.getElementById("botonCierraSesion").addEventListener("click", reestablecer);
-
-    if(parseInt(getCookieValue("visitas")) > 0){
-        // Ya no se muestra el panel de las cookies y se muestra el resultado.
-        document.getElementById("panelCookies").setAttribute("hidden", "");
-        document.getElementById("resultado").removeAttribute("hidden");
-
-        // Se le suma a uno la cantidad de visitas.
-        aumentarContadorVisitas();
     }
 }
