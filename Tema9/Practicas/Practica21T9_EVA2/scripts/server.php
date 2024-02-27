5fd9c945fc028ac9a5e4a03d881ad1f9e1e220c9
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-02-27 14:14:06 +0100

    Servidor listo

--- Tema9/Practicas/Practica21T9_EVA2/scripts/server.php
+++ Tema9/Practicas/Practica21T9_EVA2/scripts/server.php
@@ -17,58 +17,38 @@
         )
     );
 
-    if(isset($_GET["peticion"]) && isset($_GET["peticion"]["email"]) && isset($_GET["peticion"]["password"])){
-        $roleUsuarioEncontrado = "";
 
+    if(isset($_GET["email"]) && isset($_GET["password"])){ 
+        // Creamos un array para almacenar la información del usuario.
+        $usuarioEncontrado = array();
+
+        // Recorremos la matriz de usuarios.
         foreach($usuarios as $usuario){
-            if(($usuario["email"] == $_GET["peticion"]["email"]) && ($usuario["password"] == $_GET["password"])){
-                $roleUsuarioEncontrado = $usuario["role"];
+
+            // Si se ha encontrado el usuario se guarda toda su información en el nuevo array a devolver.
+            if(($usuario["email"] == $_GET["email"]) && ($usuario["password"] == $_GET["password"])){
+                $usuarioEncontrado = $usuario;
             }
         }
 
-        if($roleUsuarioEncontrado != ""){
-            echo $roleUsuarioEncontrado;
+        //Indicamos que se va a indicar un json en formato texto.
+        header("Content-Type: application/json");
+
+        // Tanto si es administrador o usuario se devuelve su informacion codificada en json.
+        // En caso de no haber encontrado el usuario se crea un array específico para devolverlo también en json.
+        if(count($usuarioEncontrado) > 0){
+            echo json_encode($usuarioEncontrado, true);
         }else{
-            echo "Usuario no reconocido";
+            $usuarioNoReconocido = 
+            array(
+                "email" => $_GET["email"],
+                "password" => "",
+                "role" => "Usuario_no_reconocido"
+            );
+
+            echo json_encode($usuarioNoReconocido, true);
         }
-    }
-?>
-
-<?php
-    $usuarios = array(
-        array(
-            "email" => "jorgemugar12@hotmail.com",
-            "password" => "000000",
-            "role" => "Administrador"
-        ),
-        array(
-            "email" => "10113@cifpceuta.es",
-            "password" => "000000",
-            "role" => "Usuario_estandar"
-        ),
-        array(
-            "email" => "secretaria@cifpceuta.es",
-            "password" => "000000",
-            "role" => "Usuario_no_reconocido"
-        )
-    );
-
-
-    if(isset($_GET["peticion"]) && isset($_GET["peticion"]["email"]) && isset($_GET["peticion"]["password"])){
-        $roleUsuarioEncontrado = "";
-
-        foreach($usuarios as $usuario){
-            echo $usuario;
-            
-            if(($usuario["email"] == "jorgemugar12@hotmail.com") && ($usuario["password"] == "000000")){
-                $roleUsuarioEncontrado = "Administrador";
-            }
-        }
-
-        if($roleUsuarioEncontrado != ""){
-            echo $roleUsuarioEncontrado;
-        }else{
-            echo "Usuario no reconocido";
-        }
+    }else{
+        echo "No se han obtenido los datos necesarios correctamente.";
     }
 ?>
